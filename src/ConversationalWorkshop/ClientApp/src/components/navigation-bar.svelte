1d5302f1a19e2573880d4ba6d86740b53e27543c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-09-06 14:37:24 +0200

    Add links in navigation bar

--- src/ConversationalWorkshop/ClientApp/src/components/navigation-bar.svelte
+++ src/ConversationalWorkshop/ClientApp/src/components/navigation-bar.svelte
@@ -12,6 +12,30 @@
 		<div class="collapse navbar-collapse" id="navbarContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+				<li class="nav-item dropdown">
+					<a
+						href="#"
+						class="nav-link dropdown-toggle"
+						role="button"
+						data-bs-toggle="dropdown"
+						aria-expanded="false">Exercises</a
+					>
+					<ul class="dropdown-menu">
+						<li>
+							<a href="/exercises/01-prompt-design/" class="dropdown-item">1. Prompt design</a>
+						</li>
+						<li>
+							<a href="/exercises/02-few-shot-learning/" class="dropdown-item"
+								>2. Few shot learning</a
+							>
+						</li>
+						<li>
+							<a href="/exercises/03-use-organization-data/" class="dropdown-item"
+								>3. Use organization data</a
+							>
+						</li>
+					</ul>
+				</li>
 			</ul>
 		</div>
 	</div>
