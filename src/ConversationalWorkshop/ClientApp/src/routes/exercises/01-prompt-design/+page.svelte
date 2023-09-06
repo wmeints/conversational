d7ca6540e6755c5521ceece87789ed2a21ce7002
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-09-06 14:31:36 +0200

    Add pages for the first two exercises

---
+++ src/ConversationalWorkshop/ClientApp/src/routes/exercises/01-prompt-design/+page.svelte
@@ -0,0 +1,5 @@
+<div class="container">
+	<div class="col">
+		<h1>Exercise 1: Prompt design</h1>
+	</div>
+</div>
---
+++ src/ConversationalWorkshop/ClientApp/src/routes/exercises/02-few-shot-learning/+page.svelte
@@ -0,0 +1,5 @@
+<div class="container">
+	<div class="col">
+		<h1>Exercise 2: Few-shot learning</h1>
+	</div>
+</div>
