65069e6268130a7cd5bd10f3270cfbf9cf64045b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-09-06 14:01:18 +0200

    Add bootstrap

--- src/ConversationalWorkshop/ClientApp/vite.config.ts
+++ src/ConversationalWorkshop/ClientApp/vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				secure: false,
+				target: 'http://localhost:5000',
+				changeOrigin: true
+			}
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
--- src/ConversationalWorkshop/Program.cs
+++ src/ConversationalWorkshop/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/api/greeting", () => "Hello there");
+
 app.MapFallbackToFile("index.html");
 app.Run();
