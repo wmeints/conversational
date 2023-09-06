6a3ca2bb48265f14e34766be63b4aff316f6b7ba
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-09-06 15:09:56 +0200

    Change description on navigation bar

--- src/ConversationalWorkshop/ClientApp/src/components/navigation-bar.svelte
+++ src/ConversationalWorkshop/ClientApp/src/components/navigation-bar.svelte
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-light">
 	<div class="container">
-		<a href="/" class="navbar-brand">CWS</a>
+		<a href="/" class="navbar-brand">APW</a>
 		<button
 			class="navbar-toggler"
 			type="button"
@@ -13,6 +13,7 @@
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item"><a href="/" class="nav-link">Home</a></li>
 				<li class="nav-item dropdown">
+					<!-- svelte-ignore a11y-invalid-attribute -->
 					<a
 						href="#"
 						class="nav-link dropdown-toggle"
