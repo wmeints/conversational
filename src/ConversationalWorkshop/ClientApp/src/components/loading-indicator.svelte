e5b39d5f54ecd42ffc26b5076f1a8309bea7077b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-09-19 19:17:45 +0200

    Change label for loading indicator

--- src/ConversationalWorkshop/ClientApp/src/components/loading-indicator.svelte
+++ src/ConversationalWorkshop/ClientApp/src/components/loading-indicator.svelte
@@ -1,4 +1,4 @@
 <div class="d-flex align-items-center">
-	<span role="status">Loading...</span>
+	<span role="status">Generating response...</span>
 	<div class="spinner-border ms-auto" aria-hidden="true" />
 </div>
